1473909e3332b58a9573da706e42f94bd9c50af0
sofia <medi61200@mail.ru>
2021-10-18 01:15:47 +0300

    basic footer

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", { delay: 900 }, gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
